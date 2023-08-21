ffa76a00e5c012a23693fe071f7c95b0747b2010
Christophe anfry <frybcn@gmail.com>
2023-08-21 14:34:24 +0200

    exercice 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function (str) {
+  const strArr = str.split('');
+  const reversedArr = strArr.reverse();
+  const result = reversedArr.join('');
 
+  if (str === '') {
+    return '';
+  }
+  return result;
 };
 
 // Do not edit below this line
