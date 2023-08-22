87a51785412dd11cff05a49b5f03aec72148afbb
Christophe anfry <frybcn@gmail.com>
2023-08-22 13:51:44 +0200

    finishing exercice

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,24 @@
-const removeFromArray = function() {
+// const removeFromArray = function (arr, ...args) {
+//   const newArray = [];
+//   arr.forEach((item) => {
+//     if (!args.includes(item)) {
+//       newArray.push(item);
+//     }
+//   });
+//   return newArray;
+// };
 
+// // Do not edit below this line
+// module.exports = removeFromArray;
+
+const removeFromArray = function (arr, ...args) {
+  if (typeof arguments[2] === NaN) {
+    return;
+  }
+  if (!args in arr) {
+    return;
+  }
+  return arr.filter((item) => !args.includes(item));
 };
 
 // Do not edit below this line
